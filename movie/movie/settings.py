195b7a19cc80c4b1b61710b5c61fb12ce79fd203
eleensmathew <92302620+eleensmathew@users.noreply.github.com>
2023-06-05 13:58:22 +0530

    Update settings.py

--- movie/movie/settings.py
+++ movie/movie/settings.py
@@ -165,6 +165,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'hellouserhi1@gmail.com'
-EMAIL_HOST_PASSWORD = 'rfblwhvwxpjgcjrf'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'hellouserhi1@gmail.com'
